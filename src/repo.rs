60955e2497f98711b4836e468b7f3b274b5611b2
Weiyi Chen <wychen325@gmail.com>
2022-07-06 21:58:02 +0800

    feat: add support of parsing repos from repo config file

--- src/repo.rs
+++ src/repo.rs
@@ -1,6 +1,9 @@
 use std::io::Read;
+use std::path::Path;
+use std::process::Command;
 
 use anyhow::{Context, Result};
+use configparser;
 use flate2::read::GzDecoder;
 use quick_xml;
 use reqwest;
@@ -66,6 +69,8 @@ struct Package {
 struct Repo {
     #[serde(rename = "package")]
     packages: Vec<Package>,
+    #[serde(skip)]
+    name: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -86,7 +91,7 @@ struct Location {
 }
 
 impl Repo {
-    fn from_url(repo_url: String) -> Result<Repo> {
+    fn from_baseurl(repo_url: String) -> Result<Repo> {
         // Get repomd.xml from the repo.
         let repomd_url = repo_url.clone() + "repodata/repomd.xml";
         let repomd_xml = reqwest::blocking::get(&repomd_url)
@@ -114,6 +119,71 @@ impl Repo {
         primary_gz.read_to_string(&mut primary_xml)?;
         quick_xml::de::from_str(&primary_xml).with_context(|| "Failed to parse primary.xml")
     }
+
+    // Read the .repo config file at path,
+    // then return a vector of repos in the file.
+    fn from_file(path: &Path) -> Result<Vec<Repo>> {
+        let mut repos: Vec<Repo> = Vec::new();
+        // Parse .repo config file into a map.
+        let mut config = configparser::ini::Ini::new_cs();
+        let map = config.load(path.to_str().unwrap()).unwrap();
+        // Iterate each repo.
+        for (_, kvs) in map {
+            let mut repo_name = String::new();
+            let mut repo_baseurl = String::new();
+            for (key, value) in kvs {
+                match key.trim() {
+                    "name" => {
+                        repo_name = value.unwrap();
+                    }
+                    "baseurl" => {
+                        repo_baseurl = value.unwrap();
+                    }
+                    "mirrorlist" => {
+                        // To be done...
+                    }
+                    _ => (),
+                }
+            }
+            // Replace yum variables.
+            //
+            // $basearch refers to the base architecture of the system.
+            // For example, i686 machines have a base architecture of i386,
+            // and AMD64 and Intel 64 machines have a base architecture of x86_64.
+            if repo_baseurl.contains("$basearch") {
+                let mut basearch = String::from_utf8(Command::new("arch").output()?.stdout)?;
+                if basearch == "i686" {
+                    basearch = String::from("i386");
+                }
+                repo_baseurl = repo_baseurl.replace("$basearch", &basearch);
+            }
+            // $arch refers to the system's CPU architecture.
+            if repo_baseurl.contains("$arch") {
+                let arch = String::from_utf8(Command::new("arch").output()?.stdout)?;
+                repo_baseurl = repo_baseurl.replace("$arch", &arch);
+            }
+            // $releasever refers to the release version of the system.
+            // Yum obtains the value of $releasever from the distroverpkg=value line in the /etc/yum.conf configuration file.
+            // If there is no such line in /etc/yum.conf,
+            // then yum infers the correct value by deriving the version number from the system-release package.
+            if repo_baseurl.contains("$releasever") {
+                let release = String::from_utf8(
+                    Command::new("rpm")
+                        .args(["-q", "openEuler-release"])
+                        .output()?
+                        .stdout,
+                )
+                .with_context(|| "System-release package not found")?;
+                let release: Vec<&str> = release.split("-").collect();
+                let releasever = release[2];
+                repo_baseurl = repo_baseurl.replace("$releasever", releasever);
+            }
+            let mut repo = Repo::from_baseurl(repo_baseurl)?;
+            repo.name = repo_name;
+            repos.push(repo);
+        }
+        Ok(repos)
+    }
 }
 
 #[cfg(test)]
@@ -123,7 +193,7 @@ mod tests {
     #[test]
     fn test_parse_primary_xml() -> Result<()> {
         let repo_url = String::from("https://repo.openeuler.org/openEuler-22.03-LTS/OS/x86_64/");
-        let repo: Repo = Repo::from_url(repo_url)?;
+        let repo: Repo = Repo::from_baseurl(repo_url)?;
         println!("{:?}", repo.packages);
         Ok(())
     }
